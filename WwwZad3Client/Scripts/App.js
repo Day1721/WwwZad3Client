bf84cd712c89cacee91ef7a1f319913604230216
Day1721 <puzynamarek97@gmail.com>
2017-05-12 01:07:00 +0200

    Add project files.

---
+++ WwwZad3Client/Scripts/App.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict';
+
+    const app = angular.module('App', ['ngRoute']);
+
+    app.config(configFunc);
+    app.run(runFunc);
+
+    runFunc.$inject = ['$rootScope']
+})();
